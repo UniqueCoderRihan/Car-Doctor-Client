9107271d995a3d0d52bab066e7c8a742b14496ae
Rihanul Islam <rihanthedev@gmail.com>
2023-05-14 01:23:02 +0600

    Nabvar and Footer Page Layout Setup

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Nabvar from '../Pages/Shared/Nabvar/Nabvar';
 
 const Main = () => {
     return (
         <div>
+            <Nabvar></Nabvar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
