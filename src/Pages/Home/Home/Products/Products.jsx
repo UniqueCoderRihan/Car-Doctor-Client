6336a3cb70b867124668ac83bd4320c9f414ecaf
Rihanul Islam <rihanthedev@gmail.com>
2023-05-14 21:16:22 +0600

    Rating added Static

--- src/Pages/Home/Home/Products/Products.jsx
+++ src/Pages/Home/Home/Products/Products.jsx
@@ -1,12 +1,15 @@
-import { Rating } from '@mui/material';
+import { Rating, Typography } from '@mui/material';
 import product1 from '../../../../assets/images/products/1.png';
 import product2 from '../../../../assets/images/products/2.png';
 import product3 from '../../../../assets/images/products/3.png';
 import product4 from '../../../../assets/images/products/4.png';
 import product5 from '../../../../assets/images/products/5.png';
 import product6 from '../../../../assets/images/products/6.png';
+import { useState } from 'react';
 
 const Products = () => {
+    const [value, setValue] =useState(4);
+    
     return (
         <div>
             <div className="text-center my-2">
@@ -17,47 +20,11 @@ const Products = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {/* Static Product Showed every Card  */}
                 <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img className='w-3/4' src={product1} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
-                        <Rating
-                        name="read-only"></Rating>
-                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
-                    </div>
-                </div>
-                <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img className='w-3/4' src={product2} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
-                        <Rating
-                        name="read-only"></Rating>
-                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
-                    </div>
-                </div>
-                <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img className='w-3/4' src={product3} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
-                        <Rating
-                        name="read-only"></Rating>
-                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
-                    </div>
-                </div>
-                <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img className='w-3/4' src={product4} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
-                        <Rating
-                        name="read-only"></Rating>
-                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
-                    </div>
-                </div>
-                <div className="card card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img className='w-3/4' src={product5} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
-                        <Rating
-                        name="read-only"></Rating>
+    
+      <Rating name="read-only" value={value} readOnly />
                         <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
                     </div>
                 </div>
@@ -65,8 +32,39 @@ const Products = () => {
                     <figure><img className='w-3/4' src={product6} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
-                        <Rating
-                        name="read-only"></Rating>
+                        <Rating name="read-only" value={value} readOnly />
+                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
+                    </div>
+                </div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='w-3/4' src={product4} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
+                        <Rating name="read-only" value={value} readOnly />
+                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
+                    </div>
+                </div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='w-3/4' src={product4} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
+                        <Rating name="read-only" value={value} readOnly />
+                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
+                    </div>
+                </div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='w-3/4' src={product5} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
+                        <Rating name="read-only" value={value} readOnly />
+                        <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
+                    </div>
+                </div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='w-3/4' src={product6} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title font-bold text-center ">Car Engine Plug</h2>
+                        <Rating name="read-only" value={value} readOnly />
                         <p className='font-bold text-xl text-orange-500'>$ 20.00</p> 
                     </div>
                 </div>
