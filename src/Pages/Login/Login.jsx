9274b32bf0ca69f5f929401fb150b418876cbfba
Rihanul Islam <rihanthedev@gmail.com>
2023-05-14 22:32:40 +0600

    Firebase env setup

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,46 @@
+import img from '../../assets/images/login/login.svg'
+const Login = () => {
+    
+    const handleSubmit = event=>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className=" w-1/2 mr-12">
+                    <img className='' src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <form onSubmit={handleSubmit}>
+                            <h1 className="text-5xl font-bold">Login</h1>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" className="btn btn-primary" value="Login" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
