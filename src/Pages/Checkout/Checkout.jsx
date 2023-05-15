a377818cfaca749c73daaea0369d15a020c5ade6
Rihanul Islam <rihanthedev@gmail.com>
2023-05-15 16:12:45 +0600

    Booking Page design Done

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -2,11 +2,52 @@ import { useLoaderData } from "react-router-dom";
 
 const Checkout = () => {
     const service = useLoaderData();
-    const {title,price,_id} = service;
+    const { title, price, _id } = service;
     return (
         <div>
             <h2>Book Service: {title}</h2>
+
+            <div className="hero min-h-screen bg-base-200">
+                <form>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Frist Name</span>
+                            </label>
+                            <input type="text" name="fristName" placeholder="Frist Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Last Name</span>
+                            </label>
+                            <input type="text" placeholder="Last Name" name="lastName" className="input input-bordered" />
+
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Your Phone Number</span>
+                            </label>
+                            <input type="number" placeholder="Enter Your Phone Number" name="phone" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Enter Your Email</span>
+                            </label>
+                            <input type="email" placeholder="Email" name="email" required className="input input-bordered" />
+
+                        </div>
+                    </div>
+                    <div className="my-3">
+                        <textarea name="message" rows="8" cols="70" placeholder="Enter Your Message about Service ">
+                        </textarea>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input type="submit" value="Confirm Order" className="btn btn-primary btn-block" />
+                    </div>
+                </form>
+            </div>
         </div>
+
     );
 };
 
