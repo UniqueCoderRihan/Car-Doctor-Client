2e1832d8fd682954f4bcdb2a34fd0b20b58d7464
Rihanul Islam <rihanthedev@gmail.com>
2023-05-15 16:20:13 +0600

    Checkout

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -3,12 +3,23 @@ import { useLoaderData } from "react-router-dom";
 const Checkout = () => {
     const service = useLoaderData();
     const { title, price, _id } = service;
+    const handleSubmit=event=>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const fristName = form.fristName.value;
+        const lastName = form.lastName.value;
+        const phone = form.phone.value;
+        const message = form.message.value;
+        const name= fristName+''+ lastName;
+        console.log(name,phone,email,message);
+    }
     return (
         <div>
             <h2>Book Service: {title}</h2>
 
             <div className="hero min-h-screen bg-base-200">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div className="form-control">
                             <label className="label">
